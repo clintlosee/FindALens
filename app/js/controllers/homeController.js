5fe60a98863acbb0819b6810864ec0e15f53cffb
Clint Losee <clint.losee@gmail.com>
2014-12-08 21:08:08 -0700

    Modified home page full lens search feature.

--- app/js/controllers/homeController.js
+++ app/js/controllers/homeController.js
@@ -1,7 +1,9 @@
-FindALens.controller('HomeController', function($scope, CanonLens, NikonLens) {
+FindALens.controller('HomeController', function($scope, $routeParams, CanonLens, NikonLens) {
   $scope.message = 'Welcome to Find-A-Lens';
 
   $scope.canonLenses = CanonLens.query();
   $scope.nikonLenses = NikonLens.query();
+
+  $scope.lensId = $routeParams.lensId;
 
 });
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,11 +2,13 @@ FindALens.controller('CanonController', function($scope) {
   $scope.message = 'This is the Canon Controller';
 });
 
-FindALens.controller('HomeController', function($scope, CanonLens, NikonLens) {
+FindALens.controller('HomeController', function($scope, $routeParams, CanonLens, NikonLens) {
   $scope.message = 'Welcome to Find-A-Lens';
 
   $scope.canonLenses = CanonLens.query();
   $scope.nikonLenses = NikonLens.query();
+
+  $scope.lensId = $routeParams.lensId;
 
 });
 
